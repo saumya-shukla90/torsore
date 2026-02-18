e6c3520d83ec947f53c301e12d9c1ddcba9573ea
saumyashukla790 <saumya.shukla@visionplusindia.in>
2026-02-18 14:08:50 +0530

    up

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -67,7 +67,7 @@ export function Navbar() {
     <header
       className={cn(
         "fixed top-0 left-0 right-0 z-50 transition-all duration-300",
-        isScrolled ? "bg-black-900 shadow-soft text-white" : "bg-black/5 backdrop-blur-md text-white"
+        isScrolled ? "bg-secondary shadow-soft text-white" : "bg-black/5 backdrop-blur-md text-white"
       )}
     >
       <nav className="container mx-auto px-4 lg:px-8 relative" onMouseLeave={() => setActiveMega(null)}>
